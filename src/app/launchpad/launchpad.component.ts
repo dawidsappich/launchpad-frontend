4eceacaa45d48ff27705fa23e84fa5a81068e7d9
dawid sappich <dawid.sappich@cdi-ag.de>
2020-02-03 15:12:17 +0100

    load launchpad after login

--- src/app/launchpad/launchpad.component.ts
+++ src/app/launchpad/launchpad.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {LaunchpadService} from './service/launchpad.service';
+import {Launchpad} from '../model/launchpad.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-launchpad',
   templateUrl: './launchpad.component.html',
   styleUrls: ['./launchpad.component.scss']
 })
-export class LaunchpadComponent implements OnInit {
+export class LaunchpadComponent implements OnInit, OnDestroy {
+  private launchpad: Launchpad;
+  private launchpadSubscription$: Subscription;
 
-  constructor() { }
+  constructor(private launchpadService: LaunchpadService) { }
 
   ngOnInit() {
-    // TODO load launchpad
+    this.launchpadService.launchpad$.subscribe(launchpad => this.launchpad = launchpad);
+    this.launchpadService.loadLaunchPad();
+  }
+
+  ngOnDestroy(): void {
+    this.launchpadSubscription$ ? this.launchpadSubscription$.unsubscribe() : this.launchpadSubscription$ = null;
   }
 
 }
